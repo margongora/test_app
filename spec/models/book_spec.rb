f30e965fac5c37550fd8c5844188538cbe97c284
margongora <margongora0803@gmail.com>
2025-01-28 19:00:22 +0000

    Passed all written tests with TDD

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,13 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it 'is valid with a title' do
-    book = Book.new(title: 'Moby Dick')
+  it 'is valid with a title, author, date published, and price' do
+    book = Book.new(title: 'Moby Dick', author: 'Herman Melville', date_published: Date.new(1851), price: 20)
     expect(book).to be_valid
   end
 
   it 'is not valid with empty title' do 
-    book = Book.new
+    book = Book.new(author: 'Herman Melville', date_published: Date.new(1851), price: 20)
     expect(book).to_not be_valid
   end
+
+  it 'is invalid without an author' do
+    book = Book.new(title: 'Moby Dick', date_published: Date.new(1851), price: 20)
+    expect(book).to_not be_valid
+  end
+
+  it 'is invalid without a date' do
+    book = Book.new(title: 'Moby Dick', author: 'Herman Melville', price: 20)
+    expect(book).to_not be_valid
+  end
+
+  it 'is invalid without a price' do
+    book = Book.new(title: 'Moby Dick', author: 'Herman Melville', date_published: Date.new(1851))
+    expect(book).to_not be_valid
+  end
+
 end
--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -5,10 +5,14 @@ RSpec.describe "Books", type: :system do
     driven_by(:rack_test)
   end
 
-  it 'creates a new book with a title' do
+  it 'creates a valid book' do
     visit new_book_path
 
     fill_in 'book_title', with: 'Moby Dick'
+    fill_in 'book_author', with: 'Herman Melville'
+    select '1851', from: 'book_date_published'
+    fill_in 'book_price', with: 20
+
     click_button 'Create Book'
 
     expect(page).to have_content('Book was successfully created.')
@@ -17,8 +21,50 @@ RSpec.describe "Books", type: :system do
   it 'fails to create a book with no title' do
     visit new_book_path
 
+    fill_in 'book_author', with: 'Herman Melville'
+    select '1851', from: 'book_date_published'
+    fill_in 'book_price', with: 20
+
     click_button 'Create Book'
 
     expect(page).to have_content('Title can\'t be blank')
   end
+
+  it 'fails to create a book without author' do
+    visit new_book_path
+
+    fill_in 'book_title', with: 'Moby Dick'
+    select '1851', from: 'book_date_published'
+    fill_in 'book_price', with: 20
+
+    click_button 'Create Book'
+
+    expect(page).to have_content('Author can\'t be blank')
+  end
+
+  # Having the default will make it so there is always a date given from the UI
+  # it 'fails to create a book without date' do
+  #   visit new_book_path
+
+  #   fill_in 'book_title', with: 'Moby Dick'
+  #   fill_in 'book_author', with: 'Herman Melville'
+  #   fill_in 'book_price', with: 20
+
+  #   click_button 'Create Book'
+
+  #   expect(page).to have_content('Date published can\'t be blank')
+  # end
+
+  it 'fails to create a book without price' do
+    visit new_book_path
+
+    fill_in 'book_title', with: 'Moby Dick'
+    fill_in 'book_author', with: 'Herman Melville'
+    select '1851', from: 'book_date_published'
+
+    click_button 'Create Book'
+
+    expect(page).to have_content('Price can\'t be blank')
+  end
+
 end
